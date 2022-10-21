bb4d0df4f3b4556e6e8fde4595e2437ff606fa97
Marcos Díaz <marcosdiazferrera@gmail.com>
2022-10-21 16:22:06 +0200

    Añadimos descripciones

--- personas_proyecto1.py
+++ personas_proyecto1.py
@@ -1,23 +1,23 @@
 import csv
-
-#creamos la funcion que nos permite leer el fichero entero,saltandose la cabecera separando cada una de las columnas por el punto y coma
-
+from collections import namedtuple
+'''creamos la funcion que nos permite leer el fichero entero,saltandose la cabecera separando cada una de las columnas por el punto y coma'''
+Registros=namedtuple('Registros','numero nombre apellidos gender pais fecha_nacimiento altura peso hobbies')
 def leer_fichero_entero(fichero):
+    '''
+    Devuelve una lista de tuplas de tipo Registro a partir de los datos incluidos en el fichero
+    csv dado como parámetro. El fichero debe estar codificado en formato utf-8.
+     
+    @param fichero: Nombre y ruta del fichero csv a leer. 
+    @type fichero:srt
+    @return: Una lista de tuplas de tipo Registros con todos los datos del csv
+     @rtype: [ Registros(int, str, boolean, int, str. boolean, str, str, in, boolean, int, boolean, int, int, datetime.date)]  
+    '''
     with open(fichero,encoding='utf-8') as f:
         lector=csv.reader(f, delimiter=';')
         next(lector)
         datos_personas=[]
-        for datos in lector:
-            numero=int(datos[0])
-            nombre=datos[1]
-            apellidos=datos[2]
-            gender=datos[3]
-            pais=datos[4]
-            fecha_nacimiento=datos[5]
-            altura=float(datos[6].replace(',','.'))
-            peso=int(datos[7])
-            hobbies=(tuple(datos[8].split(',')))
-            tupla_datos=(numero,nombre,apellidos,gender,pais,fecha_nacimiento,altura,peso,hobbies)
+        for numero,nombre,apellidos,gender,pais,fecha_nacimiento,altura,peso,hobbies in lector:
+            tupla_datos=Registros(int(numero),nombre,apellidos,gender,pais,fecha_nacimiento,float(altura.replace(',','.')),int(peso),tuple(hobbies.split(',')))
             datos_personas.append(tupla_datos)
     return datos_personas
  
